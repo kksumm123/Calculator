5b4e9d6e95756203f0263be92b6cc9d799ce7f36
kksumm123 <kksumm123@gmail.com>
2021-10-12 20:08:56 +0900

    버튼들 가져와서 세팅하는 로직 추가

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -30,6 +30,7 @@ namespace Calculator
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button0 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -39,11 +40,26 @@ namespace Calculator
             this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
-            this.button10 = new System.Windows.Forms.Button();
-            this.button11 = new System.Windows.Forms.Button();
-            this.button12 = new System.Windows.Forms.Button();
+            this.buttonPlus = new System.Windows.Forms.Button();
+            this.buttonEqual = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // button0
+            // 
+            this.button0.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button0.BackgroundImage")));
+            this.button0.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button0.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button0.FlatAppearance.BorderSize = 0;
+            this.button0.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button0.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button0.ForeColor = System.Drawing.Color.White;
+            this.button0.Location = new System.Drawing.Point(25, 527);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(50, 46);
+            this.button0.TabIndex = 0;
+            this.button0.Text = "0";
+            this.button0.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
@@ -54,11 +70,11 @@ namespace Calculator
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(25, 527);
+            this.button1.Location = new System.Drawing.Point(25, 464);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(50, 46);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "0";
+            this.button1.TabIndex = 1;
+            this.button1.Text = "1";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
@@ -70,11 +86,11 @@ namespace Calculator
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(25, 464);
+            this.button2.Location = new System.Drawing.Point(87, 464);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(50, 46);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "1";
+            this.button2.TabIndex = 2;
+            this.button2.Text = "2";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
@@ -86,11 +102,11 @@ namespace Calculator
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button3.ForeColor = System.Drawing.Color.White;
-            this.button3.Location = new System.Drawing.Point(87, 464);
+            this.button3.Location = new System.Drawing.Point(152, 464);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(50, 46);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "2";
+            this.button3.TabIndex = 3;
+            this.button3.Text = "3";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
@@ -102,11 +118,11 @@ namespace Calculator
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button4.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button4.ForeColor = System.Drawing.Color.White;
-            this.button4.Location = new System.Drawing.Point(152, 464);
+            this.button4.Location = new System.Drawing.Point(25, 402);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(50, 46);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "3";
+            this.button4.TabIndex = 4;
+            this.button4.Text = "4";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button5
@@ -118,11 +134,11 @@ namespace Calculator
             this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button5.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button5.ForeColor = System.Drawing.Color.White;
-            this.button5.Location = new System.Drawing.Point(25, 402);
+            this.button5.Location = new System.Drawing.Point(87, 402);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(50, 46);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "4";
+            this.button5.TabIndex = 5;
+            this.button5.Text = "5";
             this.button5.UseVisualStyleBackColor = true;
             // 
             // button6
@@ -134,11 +150,11 @@ namespace Calculator
             this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button6.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button6.ForeColor = System.Drawing.Color.White;
-            this.button6.Location = new System.Drawing.Point(87, 402);
+            this.button6.Location = new System.Drawing.Point(152, 402);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(50, 46);
-            this.button6.TabIndex = 5;
-            this.button6.Text = "5";
+            this.button6.TabIndex = 6;
+            this.button6.Text = "6";
             this.button6.UseVisualStyleBackColor = true;
             // 
             // button7
@@ -150,11 +166,11 @@ namespace Calculator
             this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button7.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button7.ForeColor = System.Drawing.Color.White;
-            this.button7.Location = new System.Drawing.Point(152, 402);
+            this.button7.Location = new System.Drawing.Point(25, 340);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(50, 46);
-            this.button7.TabIndex = 6;
-            this.button7.Text = "6";
+            this.button7.TabIndex = 7;
+            this.button7.Text = "7";
             this.button7.UseVisualStyleBackColor = true;
             // 
             // button8
@@ -166,11 +182,11 @@ namespace Calculator
             this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button8.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button8.ForeColor = System.Drawing.Color.White;
-            this.button8.Location = new System.Drawing.Point(25, 340);
+            this.button8.Location = new System.Drawing.Point(87, 340);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(50, 46);
-            this.button8.TabIndex = 7;
-            this.button8.Text = "7";
+            this.button8.TabIndex = 8;
+            this.button8.Text = "8";
             this.button8.UseVisualStyleBackColor = true;
             // 
             // button9
@@ -182,61 +198,45 @@ namespace Calculator
             this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button9.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button9.ForeColor = System.Drawing.Color.White;
-            this.button9.Location = new System.Drawing.Point(87, 340);
+            this.button9.Location = new System.Drawing.Point(152, 340);
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(50, 46);
-            this.button9.TabIndex = 8;
-            this.button9.Text = "8";
+            this.button9.TabIndex = 9;
+            this.button9.Text = "9";
             this.button9.UseVisualStyleBackColor = true;
             // 
-            // button10
+            // buttonPlus
             // 
-            this.button10.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button10.BackgroundImage")));
-            this.button10.Cursor = System.Windows.Forms.Cursors.Default;
-            this.button10.FlatAppearance.BorderColor = System.Drawing.Color.White;
-            this.button10.FlatAppearance.BorderSize = 0;
-            this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button10.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button10.ForeColor = System.Drawing.Color.White;
-            this.button10.Location = new System.Drawing.Point(152, 340);
-            this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(50, 46);
-            this.button10.TabIndex = 9;
-            this.button10.Text = "9";
-            this.button10.UseVisualStyleBackColor = true;
+            this.buttonPlus.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonPlus.BackgroundImage")));
+            this.buttonPlus.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonPlus.Cursor = System.Windows.Forms.Cursors.Default;
+            this.buttonPlus.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.buttonPlus.FlatAppearance.BorderSize = 0;
+            this.buttonPlus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonPlus.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonPlus.ForeColor = System.Drawing.Color.White;
+            this.buttonPlus.Location = new System.Drawing.Point(225, 464);
+            this.buttonPlus.Name = "buttonPlus";
+            this.buttonPlus.Size = new System.Drawing.Size(50, 109);
+            this.buttonPlus.TabIndex = 10;
+            this.buttonPlus.Text = "+";
+            this.buttonPlus.UseVisualStyleBackColor = true;
             // 
-            // button11
+            // buttonEqual
             // 
-            this.button11.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button11.BackgroundImage")));
-            this.button11.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button11.Cursor = System.Windows.Forms.Cursors.Default;
-            this.button11.FlatAppearance.BorderColor = System.Drawing.Color.White;
-            this.button11.FlatAppearance.BorderSize = 0;
-            this.button11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button11.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button11.ForeColor = System.Drawing.Color.White;
-            this.button11.Location = new System.Drawing.Point(225, 464);
-            this.button11.Name = "button11";
-            this.button11.Size = new System.Drawing.Size(50, 109);
-            this.button11.TabIndex = 10;
-            this.button11.Text = "+";
-            this.button11.UseVisualStyleBackColor = true;
-            // 
-            // button12
-            // 
-            this.button12.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button12.BackgroundImage")));
-            this.button12.Cursor = System.Windows.Forms.Cursors.Default;
-            this.button12.FlatAppearance.BorderColor = System.Drawing.Color.White;
-            this.button12.FlatAppearance.BorderSize = 0;
-            this.button12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button12.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button12.ForeColor = System.Drawing.Color.White;
-            this.button12.Location = new System.Drawing.Point(288, 527);
-            this.button12.Name = "button12";
-            this.button12.Size = new System.Drawing.Size(50, 46);
-            this.button12.TabIndex = 11;
-            this.button12.Text = "=";
-            this.button12.UseVisualStyleBackColor = true;
+            this.buttonEqual.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonEqual.BackgroundImage")));
+            this.buttonEqual.Cursor = System.Windows.Forms.Cursors.Default;
+            this.buttonEqual.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.buttonEqual.FlatAppearance.BorderSize = 0;
+            this.buttonEqual.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonEqual.Font = new System.Drawing.Font("맑은 고딕", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.buttonEqual.ForeColor = System.Drawing.Color.White;
+            this.buttonEqual.Location = new System.Drawing.Point(288, 527);
+            this.buttonEqual.Name = "buttonEqual";
+            this.buttonEqual.Size = new System.Drawing.Size(50, 46);
+            this.buttonEqual.TabIndex = 11;
+            this.buttonEqual.Text = "=";
+            this.buttonEqual.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
@@ -256,9 +256,8 @@ namespace Calculator
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(359, 606);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button12);
-            this.Controls.Add(this.button11);
-            this.Controls.Add(this.button10);
+            this.Controls.Add(this.buttonEqual);
+            this.Controls.Add(this.buttonPlus);
             this.Controls.Add(this.button9);
             this.Controls.Add(this.button8);
             this.Controls.Add(this.button7);
@@ -268,6 +267,7 @@ namespace Calculator
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.button0);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "Form1";
             this.Text = "kksumm123@gmail.com";
@@ -277,6 +277,7 @@ namespace Calculator
 
         #endregion
 
+        private System.Windows.Forms.Button button0;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
@@ -286,9 +287,8 @@ namespace Calculator
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button9;
-        private System.Windows.Forms.Button button10;
-        private System.Windows.Forms.Button button11;
-        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button buttonPlus;
+        private System.Windows.Forms.Button buttonEqual;
         private System.Windows.Forms.Label label1;
     }
 }
--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -12,10 +12,39 @@ namespace Calculator
 {
     public partial class Form1 : Form
     {
+        List<Button> allButtons = new List<Button>();
+
         public Form1()
         {
             InitializeComponent();
+            allButtons.Add(button0);
+            allButtons.Add(button1);
+            allButtons.Add(button2);
+            allButtons.Add(button3);
+            allButtons.Add(button4);
+            allButtons.Add(button5);
+            allButtons.Add(button6);
+            allButtons.Add(button7);
+            allButtons.Add(button8);
+            allButtons.Add(button9);
+            allButtons.Add(buttonPlus);
+            allButtons.Add(buttonEqual);
+
+            foreach (var item in allButtons)
+            {
+                item.Click += ClickButtonEvent;
+                SetButtonInit(item);
+            }
         }
 
+        private void SetButtonInit(Button button)
+        {
+            button.Name = button.Text;
+        }
+
+        void ClickButtonEvent(object sender, EventArgs e)
+        {
+            MessageBox.Show(sender.ToString());
+        }
     }
 }
